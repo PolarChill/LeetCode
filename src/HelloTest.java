c726d9f4e63998eae9f67c140f37fa7396985e0a
PolarChill <polarhk@163.com>
2017-10-12 02:59:14 -0500

    Update HelloTest.java
    在HelloTest添加的方法

--- src/HelloTest.java
+++ src/HelloTest.java
@@ -3,5 +3,12 @@
  * Created By Polar on 2017/10/12
  */
 public class HelloTest {
+  public static void sayHello() {
+    System.out.println("hello");
+  }
+  
+  public static void main(String[] args) {
+    sayHello();
+  }
 
 }
